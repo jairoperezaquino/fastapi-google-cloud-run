5d889b98d5e0390a6546a8bc26d34c6725583397
Jairo Perez <jperaqu@gmail.com>
2024-11-30 22:02:45 -0500

    test logging

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
+import logging
+from fastapi.logger import logger
+from cloud_logging.middleware import LoggingMiddleware
+from cloud_logging.setup import setup_logging
+
+import logging
 from fastapi import FastAPI
 
 app = FastAPI()
 
+setup_logging()
+app.add_middleware(LoggingMiddleware)
+
 @app.get("/")
 async def root():
-    return f"Hello world!"
+    logger.debug('DEBUG LOG')
+    logger.error('ERROR LOG')
+    logger.warning('WARNING LOG')
+    logger.info('INFO LOG')
+    return {'message': 'Hello World'}
