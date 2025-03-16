db7e316185fb71343d34e101513856e8a84da233
Jairo Perez <jperaqu@gmail.com>
2025-03-15 20:33:59 -0500

    Enhance logging

--- app/logging/google/filter.py
+++ app/logging/google/filter.py
@@ -11,13 +11,16 @@ class GoogleCloudLogFilter(CloudLoggingFilter):
         record.http_request = http_request_context.get()
 
         trace = cloud_trace_context.get()
-        split_header = trace.split("/", 1)
+        if not trace:  # If no trace, avoid unnecessary processing
+            return super().filter(record)
 
+        split_header = trace.split("/", 1)
         record.trace = f"projects/{self.project}/traces/{split_header[0]}"
 
-        header_suffix = split_header[1]
-        record.span_id = re.findall(r"^\w+", header_suffix)[0]
+        if len(split_header) > 1:
+            header_suffix = split_header[1]
+            span_id_match = re.findall(r"^\w+", header_suffix)
+            if span_id_match:
+                record.span_id = span_id_match[0]
 
-        super().filter(record)
-
-        return True
+        return super().filter(record)
